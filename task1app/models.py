f90c5a96c1796f2560c9e5dd26fa17b8ebc74aa0
Niharika <110163537+niharikaasinghal@users.noreply.github.com>
2023-01-15 00:09:28 +0530

    Add files via upload

---
+++ task1app/models.py
@@ -0,0 +1,14 @@
+# Create your models here.
+from django.db import models
+import os
+
+# Create your models here.
+
+class person(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    profilepic = models.ImageField(upload_to='media', null=True)
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
